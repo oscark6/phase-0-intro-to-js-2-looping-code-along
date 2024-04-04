30746b6e43d0fb25085760691e0d353599d497fc
Oscar <oscarkimani86@icloud.com>
2024-04-04 10:24:08 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+function writeCards(names,eventName){
+    let thankYouMessages=[];
+
+    for(let i=0;i<names.length;i++){
+        let message='Thank you,${names[i]},for the wonderful ${eventName}gift!';
+        thankYouMessages.push(message);
+        debugger;
+    }
+    return thankYouMessages;
+}
+
+
+const names=["Charlie","Samip","Ali"];
+const eventName="birthday";
+const messages=writeCards(names,eventName);
+console.log(i);
+
+function countDown(number){
+    for (let i=number;i>=0;i--){
+        console.log(i);
+    }
+}
+
+
+countDown(10);
